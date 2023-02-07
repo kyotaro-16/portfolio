3de519190febfcee15baf1d7f7a1308ee69e8be8
kiyotaro <satokyo.2022@gmail.com>
2023-02-07 20:03:35 +0900

    TOPページの編集

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/Ramen.css') }}">
+    <title>ラーメン道</title>
+</head>
+
+<body>
+    <header>
+        <div class="header-line">
+        </div>
+        <div class="header-main">
+            <div class="header-box">
+                <a href='/Home' class="logo"><img src="{{ asset('img/ramen.png') }}" height="95px"
+                        width="343px"></a>
+            </div>
+            <div class="header-box-2">
+                <a href=''>新規登録</a>
+                <a href=''>ログイン</a>
+            </div>
+            <div class="border-1">
+            </div>
+        </div>
+        <div class="nav-icon">
+            <img src="{{ asset('img/noodle.png') }}" height="88px" width="88px">
+        </div>
+    </header>
+    <main>
+        <div class="topic">
+            <div class="topic-name">
+                <p>新着情報</p>
+                <div class="border-2"></div>
+            </div>
+            <div class="topic-box">
+                <div class="contents-box">
+                    <img class="photo" src="{{ asset('img/takana.jpg') }}">
+                    <div class="contents-main">
+                        <div class="topic-contents">
+                            <div id="contents-name">
+                                <h4>
+                                    高菜販売はじめました！
+                                </h4>
+                            </div>
+                            <div id="time">
+                                <p>2022/10/31</p>
+                            </div>
+                            <div id="topic-user">
+                                <p>@１・２・３ラーメン</p>
+                            </div>
+                        </div>
+                        <div id="contents-body">
+                            <p>
+                                毎度ありがとうございます。店内テーブルに置いております高菜をお客様のお声もあり、袋分けし販売する
+                                ことといたしました。11/20（土）より店頭にてお買い求めいただけます。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="contents-box">
+                    <img class="photo" src="{{ asset('img/takana.jpg') }}">
+                    <div class="contents-main">
+                        <div class="topic-contents">
+                            <div id="contents-name">
+                                <h4>
+                                    高菜販売はじめました！
+                                </h4>
+                            </div>
+                            <div id="time">
+                                <p>2022/10/31</p>
+                            </div>
+                            <div id="topic-user">
+                                <p>@１・２・３ラーメン</p>
+                            </div>
+                        </div>
+                        <div id="contents-body">
+                            <p>
+                                毎度ありがとうございます。店内テーブルに置いております高菜をお客様のお声もあり、袋分けし販売する
+                                ことといたしました。11/20（土）より店頭にてお買い求めいただけます。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="contents-box">
+                    <img class="photo" src="{{ asset('img/takana.jpg') }}">
+                    <div class="contents-main">
+                        <div class="topic-contents">
+                            <div id="contents-name">
+                                <h4>
+                                    高菜販売はじめました！
+                                </h4>
+                            </div>
+                            <div id="time">
+                                <p>2022/10/31</p>
+                            </div>
+                            <div id="topic-user">
+                                <p>@１・２・３ラーメン</p>
+                            </div>
+                        </div>
+                        <div id="contents-body">
+                            <p>
+                                毎度ありがとうございます。店内テーブルに置いております高菜をお客様のお声もあり、袋分けし販売する
+                                ことといたしました。11/20（土）より店頭にてお買い求めいただけます。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="blog">
+            <div class="topic-name">
+                <p>新着ブログ</p>
+                <div class="border-2"></div>
+            </div>
+            <div class="topic-box">
+                <div class="contents-box">
+                    <img class="photo" src="{{ asset('img/takana.jpg') }}">
+                    <div class="contents-main">
+                        <div class="topic-contents">
+                            <div id="contents-name">
+                                <h4>
+                                    高菜販売はじめました！
+                                </h4>
+                            </div>
+                            <div id="time">
+                                <p>2022/10/31</p>
+                            </div>
+                            <div id="topic-user">
+                                <p>@１・２・３ラーメン</p>
+                            </div>
+                        </div>
+                        <div id="contents-body">
+                            <p>
+                                毎度ありがとうございます。店内テーブルに置いております高菜をお客様のお声もあり、袋分けし販売する
+                                ことといたしました。11/20（土）より店頭にてお買い求めいただけます。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="contents-box">
+                    <img class="photo" src="{{ asset('img/takana.jpg') }}">
+                    <div class="contents-main">
+                        <div class="topic-contents">
+                            <div id="contents-name">
+                                <h4>
+                                    高菜販売はじめました！
+                                </h4>
+                            </div>
+                            <div id="time">
+                                <p>2022/10/31</p>
+                            </div>
+                            <div id="topic-user">
+                                <p>@１・２・３ラーメン</p>
+                            </div>
+                        </div>
+                        <div id="contents-body">
+                            <p>
+                                毎度ありがとうございます。店内テーブルに置いております高菜をお客様のお声もあり、袋分けし販売する
+                                ことといたしました。11/20（土）より店頭にてお買い求めいただけます。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="contents-box">
+                    <img class="photo" src="{{ asset('img/takana.jpg') }}">
+                    <div class="contents-main">
+                        <div class="topic-contents">
+                            <div id="contents-name">
+                                <h4>
+                                    高菜販売はじめました！
+                                </h4>
+                            </div>
+                            <div id="time">
+                                <p>2022/10/31</p>
+                            </div>
+                            <div id="topic-user">
+                                <p>@１・２・３ラーメン</p>
+                            </div>
+                        </div>
+                        <div id="contents-body">
+                            <p>
+                                毎度ありがとうございます。店内テーブルに置いております高菜をお客様のお声もあり、袋分けし販売する
+                                ことといたしました。11/20（土）より店頭にてお買い求めいただけます。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="footer-line"></div>
+    </footer>
+</body>
+
+</html>
